803a952cc443df6e1992516c8278bd58df30358d
maronnjapan <tihoutaikai20@gmail.com>
2024-12-21 10:37:09 +0900

    トークン判定条件追加

--- src/app/api/oauth/token/route.ts
+++ src/app/api/oauth/token/route.ts
@@ -1,4 +1,5 @@
 import { getAuth } from "@/lib/services/auth.service";
+import { getClientById } from "@/lib/services/client.service";
 import { validateChallenge } from "@/lib/services/pcke.service";
 import { getState } from "@/lib/services/state.service";
 import { deleteCode, generateIdToken, IdTokenPayload, validCode } from "@/lib/services/token.service";
@@ -10,20 +11,25 @@ const corsHeaders = {
 }
 
 export async function POST(request: NextRequest) {
-    const { code, state, code_verifier } = await request.json() as { code: string, state: string, code_verifier: string }
+    const { code, state, code_verifier, client_id, grant_type, redirect_uri } = await request.json() as { code: string, state: string, code_verifier: string, client_id: string, grant_type: 'authorization_code', redirect_uri?: string }
 
     if (!code || !state || !code_verifier) {
         return NextResponse.json({ message: 'Bad Request' }, { status: 400 })
     }
     const auth = getAuth(state + code_verifier)
-    if (!auth) {
+    const client = getClientById(client_id)
+    if (!auth || !client) {
         return NextResponse.json({ message: 'Bad Request' }, { status: 400 })
     }
 
     const isValidState = !!getState(state)
     const isValidCodeVerifier = validateChallenge(code_verifier);
     const isValidCode = validCode(code)
-    if (!isValidState || !isValidCodeVerifier || !isValidCode) {
+
+    const isValidClientId = client_id === auth.clientId
+    const isValidGrantType = grant_type === 'authorization_code'
+    const isValidRedirectUri = redirect_uri ? client.isAllowUrl(redirect_uri) : true
+    if (!isValidState || !isValidCodeVerifier || !isValidCode || !isValidClientId || !isValidGrantType || !isValidRedirectUri) {
         return NextResponse.json({ message: 'Bad Request' }, { status: 400 })
     }
 
