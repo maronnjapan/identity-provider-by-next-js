4a974540f067ffafc111f974bae7c281f0513442
maronnjapan <tihoutaikai20@gmail.com>
2024-12-22 13:40:48 +0900

    credentioalの設定解除

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,6 @@ const nextConfig: NextConfig = {
       {
         source: "/api/oauth/token",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "*" },
